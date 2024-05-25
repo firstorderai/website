7266eacb4008314ae9168807085ab24205c3ef70
kenzo.ai <kenzo@firstorder.ai>
2024-05-25 22:39:09 +0800

    fix redirect subdomain

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,9 +2,17 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+function subdomain(host: string, subdomain: string): string {
+  if (host.startsWith(subdomain + '.')) {
+    return host;
+  } else {
+    return subdomain + '.' + host;
+  }
+}
+
 export async function middleware(req: NextRequest) {
   const url = req.nextUrl.clone();
-  const host = req.headers.get('host');
+  const host = req.headers.get('host') || '';
   const slug = url.pathname.split('/')[1];
 
   // console.log('host: ', host);
@@ -12,7 +20,7 @@ export async function middleware(req: NextRequest) {
   // console.log('url path: ', url.pathname);
 
   if (slug == 'authenticator') {
-    const hostname = slug + '.' + host;
+    const hostname = subdomain(host, slug);
     const pathname = url.pathname.substring('/authenticator'.length);
     const newUrl = new URL(`https://${hostname}${pathname}`);
     // console.log('redirect');
